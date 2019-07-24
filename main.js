2776cf382c343a8f4eba3e9d4b6b55b42c34ad85
Alicia Palacios Alvarado <alizz.palacios@outlook.com>
2019-07-24 15:04:36 -0500

    modificacion para el modal

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+/**HIDE Y SHOW*/
+
+const hideSection = id => document.getElementById(id).classList.add('hide');
+const showSection = id => document.getElementById(id).classList.remove('hide');
+
+
 /*BUTTONS*/
 
 const tripButtonMobile = document.getElementById('new-trip-mobile');
-const tripButtonDesktop = document.getElementById('new-trip-desktop')
+const tripButtonDesktop = document.getElementById('new-trip-desktop');
 
 const driveButtonMobile = document.getElementById('drive-btn-mobile');
 const driveButtonDesktop = document.getElementById('drive-btn-desktop');
@@ -10,10 +16,6 @@ const driverButton = document.getElementById('driver-button');
 const loginButtonDesktop = document.getElementById('login-btn-mobile');
 const loginButtonMobile = document.getElementById('login-btn-desktop');
 
-/**HIDE Y SHOW*/
-
-const hideSection = id => document.getElementById(id).classList.add('hide');
-const showSection = id => document.getElementById(id).classList.remove('hide');
 
 /*CLOSE*/
 
@@ -22,17 +24,20 @@ const closeModalCheckIn = document.getElementById('close-check-in');
 const closeModalDriver = document.getElementById('close-become-drive');
 
 /*MODAL 1*/
-const openModalNewUser = ()=>{
-    showSection ('account');
+const openModalNewUser = () => {
+    showSection ('new-account');
 }
 
 
-loginMobile.addEventListener('click', openModalNewUser);
-loginDesktop.addEventListener('click', openModalNewUser);
+tripButtonMobile.addEventListener('click', openModalNewUser);
+tripButtonDesktop.addEventListener('click', openModalNewUser);
 
 const closeModalRegister = () => {
     hideSection ('account');
 }
 
+closeModalNewUser.addEventListener('click', closeModalNewUser);
 
 /*MODAL 2*/
+
+const o
